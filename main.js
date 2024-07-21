63716865989f29512ff9b396be53da4ebb3418e8
Bernard Paetzold <bernard2412@gmail.com>
2024-07-21 14:10:11 +0200

    Continued work on scaling and background

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-let console_history = "Welcome to the VCV-2000, if you are a new user, please type --help for system instructions.\n";
+let console_history = "Welcome to the VCV-2000, if you are a new user, please type -help for system instructions.\n" +
+                        "To print the CV to the screen, use the -print command.";
 let username = "[guest-user]$ ";
 let console_text = "";
 let input_text = "";
@@ -9,6 +10,8 @@ let screenContent = document.getElementById("screen-content");
 let screen = document.getElementById("crt-screen");
 
 const fontSize = 18;
+const textSpacing = 1.2;
+const lineSpacing = 4;
 
 const rows = 100;
 const columns = 100;
@@ -33,14 +36,15 @@ setInterval(animateCursor, 1000);
 function renderText() {
     screenContent.innerHTML = ''; 
 
-    console_text = console_history + username + input_text + (cursor_visible ? '_' : "");
+    console_text = console_history + '\n' + username + input_text + (cursor_visible ? '_' : "");
 
     let xPos = 0;
     let yPos = 0;
 
     let lines = console_text.split('\n');
 
-    for (let i = 0; i < lines.length && i < Math.round(screen.getBoundingClientRect().height / fontSize); i++) {
+
+    for (let i = 0; i < lines.length && yPos < rows; i++) {
         let words = lines[i].split(' ');   
 
         for (let l = 0; l < words.length; l++) {
@@ -48,6 +52,12 @@ function renderText() {
             word.className = "console-word";
 
             const characters = words[l].split('');
+            wordWidth = characters.length * textSpacing;
+
+            if (xPos + wordWidth > columns) {
+                xPos = 0;
+                yPos += lineSpacing;
+            }
 
             for (let k = 0; k < characters.length; k++) {
                 let character = characters[k];
@@ -57,8 +67,8 @@ function renderText() {
                 wrapper.className = "text-wrapper";
                 wrapper.textContent = character;
 
-                const xPercent = (xPos + l + k + 0.5) / columns * 100;
-                const yPercent = (yPos + i + 0.5) / rows * 100 + i;
+                const xPercent = (xPos + k + 0.5) / columns * 100;
+                const yPercent = (yPos + 0.5) / rows * 100;
                 const distanceFromCenter = Math.sqrt(Math.pow((xPercent - 50) / 50, 2) + Math.pow((yPercent - 50) / 50, 2));
                 const scale = 1 - (distanceFromCenter * 0.3);
 
@@ -74,39 +84,43 @@ function renderText() {
                 rotateY(${(xPercent - 50) * -rotation}deg)
                 translateZ(${zTranslation}px)`;
 
-                wrapper.style.fontSize = fontSize;
+                wrapper.style.fontSize = `${fontSize}px`;
 
                 word.appendChild(wrapper);
 
-                wordWidth = characters.length;
-            }
-            screenContent.appendChild(word);
-            xPos += wordWidth;
+                xPos += textSpacing;
 
-            if (xPos > columns - 5) {
-                xPos = 0;
-                yPos++;
+                /*if (xPos > columns) {
+                    xPos = 0;
+                    yPos += lineSpacing;
+                }*/
             }
-
             
+            screenContent.appendChild(word);
+
+            xPos += wordWidth + textSpacing;
         }
         xPos = 0;
-        yPos++;
+        yPos += lineSpacing;
     }
 }
 
 function handleKeyPress(event) {
     if (event.key === 'Enter') {
         //Process input
-        console_history += username + input_text + '\n';
+        console_history += '\n' + username + input_text;
         switch (input_text) {
-            case "help":
-                console_history +=  "Return to main menu: main-menu" + '\n' +
-                                    "Change username: config username [new username]" + 
-                                    '\n';
+            case "":           
+                break;
+            case "-help":
+                console_history +=  "\nReturn to main menu: main-menu" + '\n' +
+                                    "Change username: config username [new username]";
+                break;
+            case "-print":
+                console_history += "\nI spent countless hours working on this fake terminal because I cannot sleep until I finish what I start.";
                 break;
             default:
-                console_history += username + "command \"" + input_text + "\" not found" + '\n';
+                console_history += '\n' + username + "command \"" + input_text + "\" not found";
                 break;
         }
         input_text = "";
@@ -115,7 +129,7 @@ function handleKeyPress(event) {
         input_text = input_text.slice(0, -1);
     } 
     else if (event.key.length === 1) {
-        input_text += event.key;
+         input_text += event.key;
 
         if (input_text.length % (columns - 15) == 0) {
             input_text += '\n';
@@ -125,3 +139,17 @@ function handleKeyPress(event) {
 }
 
 document.addEventListener('keydown', handleKeyPress);
+
+
+function calculateFontSize() {
+    return Math.max(8, Math.min(18, window.innerWidth / 100)); 
+}
+
+
+function calculateRows() {
+    return Math.floor(screen.getBoundingClientRect().height / calculateFontSize());
+}
+
+function calculateColumns() {
+    return Math.floor(screen.getBoundingClientRect().width / (calculateFontSize() * 0.6)); 
+}
